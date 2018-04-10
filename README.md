399117656e6706335e7a96b1baafe01e6eff25f6
gezdiaz <gastonezequieldiaz@outlook.com>
2018-04-10 16:00:29 -0300

    version 02 imprimir fechas y arreglo README

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Guia 2 DIED
 
 Creado el README exitosamente :+1:
+
+El sistema tiene una unica clase que imprime primero la fecha actual
+en formato d/m/aaa y luego la fecha futura dentro de 250 horas.
--- src/died/guias/guia02/app/hola2.java
+++ src/died/guias/guia02/app/hola2.java
@@ -1,14 +1,32 @@
 package died.guias.guia02.app;
+import java.util.Date;
+import java.util.GregorianCalendar;
 /**
 * @author marti
 *
 */
 public class hola2 {
 	/**
+	 * Este metodo ejecuta la aplicacion cuyo proposito sera imprimir en consola
+	 * el dia, mes y año actual.
+	 * luego sumarle 250 horas a dicha fecha e imprimir la fecha futura
 	* @param args
 	*/
 	public static void main(String[] args) {
-		System.out.println("El tiempo en milis es: "+System.currentTimeMillis());
+		Date fechaActual = new Date();
+		GregorianCalendar calendar = new GregorianCalendar();
+		calendar.setTime(fechaActual);
+		Integer dia = calendar.get(GregorianCalendar.DAY_OF_MONTH);
+		// tener en cuneta que en JAVA para la clase Calendar, el mes empieza en 0,
+		// Enero es el mes 0, no el 1
+		Integer mes = 1 + calendar.get(GregorianCalendar.MONTH);
+		Integer anio = calendar.get(GregorianCalendar.YEAR);
+		System.out.println("La fecha actual es "+dia+"/"+mes+"/"+anio);
+		calendar.add(GregorianCalendar.HOUR, 250);
+		dia = calendar.get(GregorianCalendar.DAY_OF_MONTH);
+		mes = 1 + calendar.get(GregorianCalendar.MONTH);
+		anio = calendar.get(GregorianCalendar.YEAR);
+		System.out.println("La fecha en 250 horas sera: "+dia+"/"+mes+"/"+anio);
 
 	}
 
