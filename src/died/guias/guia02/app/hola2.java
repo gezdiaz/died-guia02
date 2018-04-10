03ef027d415e6d06dccbec01c3776d05e68fe77e
gezdiaz <gastonezequieldiaz@outlook.com>
2018-04-10 15:09:22 -0300

    version inicial

---
+++ src/died/guias/guia02/app/hola2.java
@@ -0,0 +1,15 @@
+package died.guias.guia02.app;
+/**
+* @author marti
+*
+*/
+public class hola2 {
+	/**
+	* @param args
+	*/
+	public static void main(String[] args) {
+		System.out.println("El tiempo en milis es: "+System.currentTimeMillis());
+
+	}
+
+}
